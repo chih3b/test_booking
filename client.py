d367bd1798aa2bfb79b6fdec51145271cbddb952
chih3b <87154720+chih3b@users.noreply.github.com>
2024-01-18 12:16:21 +0100

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,6 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace with a secure secret key
 
 # Database initialization
 def init_db():
